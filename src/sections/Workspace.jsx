6f255420d1d0c9f1a117882054818a43bab346d6
Noah Stewart <njstewart22@icloud.com>
2024-12-17 14:59:59 -0700

    Fixing icons so they work lol

--- src/sections/Workspace.jsx
+++ src/sections/Workspace.jsx
@@ -12,29 +12,29 @@ export default function Workspace() {
           text="favourite technologies"
         />
         <div className="flex flex-wrap gap-2 justify-around ">
-          <TechIcon name="Typescript" link="https://www.typescriptlang.org" />
-          <TechIcon name="React" link="https://react.dev" />
-          <TechIcon name="Tailwind" link="https://tailwindcss.com" />
-          <TechIcon name="NextJS" link="https://nextjs.org" />
-          <TechIcon name="NodeJS" link="https://nodejs.org/en" />
-          <TechIcon name="ExpressJS" link="https://expressjs.com" />
-          <TechIcon name="Flutter" link="https://flutter.dev" />
-          <TechIcon name="Git" link="https://git-scm.com" />
+          <TechIcon name="typescript" link="https://www.typescriptlang.org" />
+          <TechIcon name="react" link="https://react.dev" />
+          <TechIcon name="tailwind" link="https://tailwindcss.com" />
+          <TechIcon name="nextjs" link="https://nextjs.org" />
+          <TechIcon name="nodejs" link="https://nodejs.org/en" />
+          <TechIcon name="expressjs" link="https://expressjs.com" />
+          <TechIcon name="flutter" link="https://flutter.dev" />
+          <TechIcon name="git" link="https://git-scm.com" />
         </div>
         <GradientText
           properties="text-lg mt-2 font-semibold from-neutral-500 via-neutral-300 to-neutral-500"
           text="for data science"
         />
         <div className="flex flex-wrap gap-2 justify-around ">
-          <TechIcon name="Python" link="https://www.python.org" />
-          <TechIcon name="Tensorflow" link="https://www.tensorflow.org" />
-          <TechIcon name="Pandas" link="https://pandas.pydata.org" />
-          <TechIcon name="Numpy" link="https://numpy.org" />
-          <TechIcon name="Seaborn" link="https://seaborn.pydata.org" />
-          <TechIcon name="Matplotlib" link="https://matplotlib.org" />
-          <TechIcon name="R" link="https://www.r-project.org" />
+          <TechIcon name="python" link="https://www.python.org" />
+          <TechIcon name="tensorflow" link="https://www.tensorflow.org" />
+          <TechIcon name="pandas" link="https://pandas.pydata.org" />
+          <TechIcon name="numpy" link="https://numpy.org" />
+          <TechIcon name="seaborn" link="https://seaborn.pydata.org" />
+          <TechIcon name="matplotlib" link="https://matplotlib.org" />
+          <TechIcon name="r" link="https://www.r-project.org" />
           <TechIcon
-            name="Excel"
+            name="excel"
             link="https://www.microsoft.com/en-ca/microsoft-365/excel"
           />
         </div>
@@ -45,21 +45,21 @@ export default function Workspace() {
         <div className="flex flex-wrap gap-2 justify-around ">
           <TechIcon name="Java" link="https://www.java.com/en/" />
           <TechIcon
-            name="Html"
+            name="html"
             link=" https://developer.mozilla.org/en-US/docs/Web/HTML"
           />
           <TechIcon
-            name="CSS"
+            name="css"
             link="https://developer.mozilla.org/en-US/docs/Web/CSS"
           />
           <TechIcon
-            name="Javascript"
+            name="javascript"
             link="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
           />
-          <TechIcon name="SQL" link="https://en.wikipedia.org/wiki/SQL" />
-          <TechIcon name="PostgreSQL" link="https://www.postgresql.org" />
-          <TechIcon name="MySQL" link="https://dev.mysql.com" />
-          <TechIcon name="Firebase" link="https://firebase.google.com" />
+          <TechIcon name="sql" link="https://en.wikipedia.org/wiki/SQL" />
+          <TechIcon name="postgresql" link="https://www.postgresql.org" />
+          <TechIcon name="mysql" link="https://dev.mysql.com" />
+          <TechIcon name="firebase" link="https://firebase.google.com" />
         </div>
       </section>
     </>
