805810fe9b468f361ae61e8443d711ab6966cb6b
Noah Stewart <njstewart22@icloud.com>
2026-06-07 14:22:38 -0300

    Updating equals sign for vercel build failing

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         className="flex flex-col justify-center items-center text-center"
       >
         <p className="text-neutral-300 text-lg font-semibold mt-16 mb-2">
-          &copy; {year == 2024 ? '2024' : `2024-${year}`} designed & developed
+          &copy; {year === 2024 ? '2024' : `2024-${year}`} designed & developed
           by Noah Stewart
         </p>
         <div className="flex flex-row gap-2 mt-4 justify-start mb-16">
